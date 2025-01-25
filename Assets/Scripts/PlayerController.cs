9fa8db064a33908755a9a2b050bf1cdf023b59ff
aLEnmeister2 <alen.studios2@gmail.com>
2025-01-24 23:50:47 -0500

    Light Attack Added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,18 +11,24 @@ public class PlayerController : MonoBehaviour
     //Movement Variables
     private Rigidbody2D rb;
 
-    private float horizontal;
     [Header("Movement Variables")]
     [SerializeField] private float movementSpeed;
     [SerializeField] private float jumpForce;
     [SerializeField] private float DashForce;
+    private float horizontal;
 
-    private bool canDash = true;
-    private bool isDashing;
+    [Header("Attack Variables")]
+    [SerializeField] public int damage;
+    [SerializeField] private GameObject bubble;
+    [SerializeField] private GameObject scratch;
+    private bool isAttacking;
+
     [Header("Dash Variables")]
     [SerializeField] private float dashForce;
     [SerializeField] private float dashingTime = 0.2f;
     [SerializeField] private float dashingCooldown = 1f;
+    private bool canDash = true;
+    private bool isDashing;
 
     private bool isFlipped = false;
 
@@ -30,55 +36,70 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        scratch.SetActive(false);
     }
 
     private void Update()
-    { 
-        //Movement 
-        horizontal = Input.GetAxisRaw("Horizontal");
-      
+    {
         //Jump
         if (Input.GetKeyDown(KeyCode.W) && stamina > 0)
         {
             //Debug.Log("Jumping");
             rb.AddForce(new Vector2(0f, jumpForce));
             stamina -= 1;
-        }   
-      
+        }
+
         //Dash
-        if (Input.GetKeyDown(KeyCode.LeftControl) && canDash)
+        if (Input.GetKeyDown(KeyCode.LeftControl) && canDash && stamina > 0)
         {
             Debug.Log("Dashing");
             StartCoroutine(Dash());
-            stamina -=1;
+            stamina -= 1;
         }
 
         //Player facing direction
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            isFlipped = false;
-        }
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            isFlipped = true;
+        if (canDash) { 
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                isFlipped = false;
+            }
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                isFlipped = true;
+            }
+
+            if (isFlipped)
+            {
+                this.transform.localRotation = Quaternion.Euler(0f, 180f, 0f);
+            }
+            else if (!isFlipped)
+            {
+                this.transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+            }
         }
 
-        if (isFlipped)
+        /*-- Attack --*/
+        if (Input.GetMouseButtonDown(0) && canDash)
         {
-            this.transform.localRotation = Quaternion.Euler(0f, 180f, 0f);
+            Debug.Log("Light Attack");
+            StartCoroutine(lightAttack());
+            
         }
-        else if (!isFlipped)
+        if (Input.GetMouseButtonDown(1) && canDash)
         {
-            this.transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+            Debug.Log("Range Attack");
+            GameObject newBubble = Instantiate(bubble);
         }
     }
 
     void FixedUpdate()
     {
-        if (isDashing)
+        if (isDashing || isAttacking)
         {
             return;
         }
+                /*-- Movement --*/
+        horizontal = Input.GetAxisRaw("Horizontal");
 
         rb.velocity = new Vector2(horizontal * movementSpeed, rb.velocity.y);
     }
@@ -110,5 +131,33 @@ public class PlayerController : MonoBehaviour
 
         yield return new WaitForSeconds(dashingCooldown);
         canDash = true;
+    }
+
+    //Light Attack
+    private IEnumerator lightAttack()
+    {
+        canDash = false;
+        isAttacking = true;
+
+        scratch.SetActive(true);
+
+        yield return new WaitForSeconds(dashingTime);
+        
+        scratch.SetActive(false);
+        isAttacking = false;
+
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
+    }
+
+    //Range Attack
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            Debug.Log("On the ground");
+            stamina = 5;
+        }
     }
 }
