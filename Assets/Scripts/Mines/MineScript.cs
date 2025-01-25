154fd793ba1ffea1a1064fd2dfb6c8b573899447
NanosHere <71567368+NanosHere@users.noreply.github.com>
2025-01-25 11:33:37 -0500

    Mines Controller
    Added functionally to base mines before adding explosion

---
+++ Assets/Scripts/Mines/MineScript.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineScript : MonoBehaviour
+{
+    public GameObject PlayerEnter;
+    public float countdown;
+    [SerializeField] GameObject ExplosionPrefab;
+    bool isExploding;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("PlayerEnter");
+            PlayerEnter = collision.gameObject;
+            //Start Countdown
+            if(isExploding == false)
+            {
+                isExploding = true;
+                ArmBomb();
+                StartCoroutine(bombCountdown());
+            }
+           
+        }
+
+    }
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            PlayerEnter = null;
+            
+
+        }
+    }
+
+
+    public void ArmBomb()
+    {
+        this.GetComponent<SpriteRenderer>().color = Color.red;
+    }
+
+
+    IEnumerator bombCountdown()
+    {
+        yield return new WaitForSeconds(countdown);
+        Instantiate(ExplosionPrefab,this.transform.position,Quaternion.identity);
+        if(PlayerEnter != null)
+        {
+            //dammagePlayer
+
+            Debug.Log("dammagePlayer");
+
+        }
+        else
+        {
+            Debug.Log("MissPLayer");
+        }
+        Destroy(this.gameObject,0.3f);
+    }
+}
