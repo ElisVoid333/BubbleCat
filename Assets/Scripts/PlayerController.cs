8291247aaa14ef1d854233859bd27eaddcdde15c
Elis Joynes <smj.joynes@gmail.com>
2025-01-26 10:55:32 -0500

    fixed jump animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -335,7 +335,7 @@ public class PlayerController : MonoBehaviour
     }
 
     /*-- Collision Functions --*/
-    private void OnCollisionStay2D(Collision2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Floor")
         {
